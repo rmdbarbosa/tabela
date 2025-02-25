5e4e7f6ccc935b74f3730c6936d902e4b1f1e61d
rmdbarbosa <r_mdbarbosa@hotmail.com>
2025-02-25 15:39:33 -0300

    feat: add Loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
+import Table from "./components/Table";
 import useEmployees from "./hooks/getEmployees";
 
 function App() {
   const { employees, loading, error } = useEmployees();
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <p className="min-h-[100svh] flex justify-center items-center">
+        Carregando...
+      </p>
+    );
   }
 
   if (error) {
@@ -14,13 +19,14 @@ function App() {
   }
 
   return (
-    <div className="bg-gray-00 min-h-[100svh]">
+    <div className={`bg-gray-00 min-h-[100svh]`}>
       <Header />
       <div className="px-[20px] xl:pl-[30px] pt-[24px]">
         <div className="flex flex-wrap justify-between items-center">
           <h1>Funcionários</h1>
           <SearchBar />
         </div>
+        <Table employees={employees} />
       </div>
     </div>
   );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,26 @@
+import { useSearchParams } from "react-router-dom";
 import Search from "../assets/Icons/Search.svg?react";
 
 export default function SearchBar() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    if (query) {
+      setSearchParams({ search: query });
+    } else {
+      setSearchParams({});
+    }
+  };
+
   return (
     <div className="flex pt-[24px] pb-[20px] relative w-full max-w-[335px]">
       <input
         type="text"
         placeholder="Pesquisar"
         className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"
+        value={searchParams.get("search") || ""}
+        onChange={handleSearch}
       />
       <button className="absolute right-3 top-1/2 transform -translate-y-1/2">
         <Search />
