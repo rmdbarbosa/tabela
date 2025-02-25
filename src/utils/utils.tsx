7205ac1ef5e0fd764f051ec6b251c23fe61f8b8f
rmdbarbosa <r_mdbarbosa@hotmail.com>
2025-02-25 15:38:25 -0300

    fix: corrigir formatação nas funções formatPhone e formatDate

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,3 +1,8 @@
-export const formatDate = (date: string) => new Date(date).toLocaleDateString();
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("pt-BR");
+};
+
 export const formatPhone = (phone: string) =>
-  phone.replace(/\D/g, "").replace(/(\d{2})(\d{4,5})(\d{4})/, "($1) $2-$3");
+  phone
+    .replace(/\D/g, "")
+    .replace(/^(\d{2})(\d{2})(\d{5})(\d{4})$/, "+$1 ($2) $3-$4");
